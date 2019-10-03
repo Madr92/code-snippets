1c98a5a7b35f677ae0b3eb46ddace59d8e44b7a0
Madr92 <56043554+Madr92@users.noreply.github.com>
2019-10-03 12:42:17 +0200

    Create tracking.py

---
+++ tutorials/tracking_ETH/tracking.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env/ python2.7
+
+import requests
+import numpy as np
+from time import sleep
+
+inputfile="PATH_TO_YOUR_ADDRESSES.TXT-FILE"
+addr=np.genfromtxt(inputfile+"addresses.txt", delimiter="\t", usecols=0, dtype=str)
+comment=np.genfromtxt(inputfile+"addresses.txt", delimiter="\t", usecols=1, dtype=str)
+
+ethbalance=[]
+
+for i in xrange(len(addr)):
+	ethbalance.append(float((requests.get("https://api.blockcypher.com/v1/eth/main/addrs/"+addr[i])).json()["balance"])/10**18)
+	sleep(5)
+	print(ethbalance[i],comment[i])
+print(np.sum(ethbalance))
